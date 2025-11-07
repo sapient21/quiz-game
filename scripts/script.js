bc81f24aee05776dd06ad5bb8477b506e2d31fac
leeq <sapientrules@gmail.com>
2025-11-07 11:40:46 +0300

    Finish

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,158 @@ const resultMessage = document.getElementById("result-message");
 const restartButton = document.getElementById("restart-btn");
 const progressBar = document.getElementById("progress");
 
+const quizQuestions = [
+  {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      { text: "Paris", correct: true },
+      { text: "Madrid", correct: false },
+    ],
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    answers: [
+      { text: "Venus", correct: false },
+      { text: "Mars", correct: true },
+      { text: "Jupiter", correct: false },
+      { text: "Saturn", correct: false },
+    ],
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    answers: [
+      { text: "Atlantic Ocean", correct: false },
+      { text: "Indian Ocean", correct: false },
+      { text: "Arctic Ocean", correct: false },
+      { text: "Pacific Ocean", correct: true },
+    ],
+  },
+  {
+    question: "Which of these is NOT a programming language?",
+    answers: [
+      { text: "Java", correct: false },
+      { text: "Python", correct: false },
+      { text: "Banana", correct: true },
+      { text: "JavaScript", correct: false },
+    ],
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    answers: [
+      { text: "Go", correct: false },
+      { text: "Gd", correct: false },
+      { text: "Au", correct: true },
+      { text: "Ag", correct: false },
+    ],
+  },
+];
+
+//quiz state vars
+let currentQuestionIndex = 0;
+let score = 0;
+let answersDisabled = false;
+
+totalQuestionsSpan.textContent = quizQuestions.length;
+maxScoreSpan.textContent = quizQuestions.length;
+
+//event listeners
+
+startButton.addEventListener('click', startQuiz)
+restartButton.addEventListener('click', restartQuiz)
+
+function startQuiz() {
+    //reset vars
+    currentQuestionIndex = 0;
+    scoreSpan.textContent = 0;
+
+    startScreen.classList.remove('active')
+    quizScreen.classList.add('active')
+
+    showQuestion()
+}
+
+function showQuestion() {
+    answersDisabled = false;
+    const currentQuestion = quizQuestions[currentQuestionIndex];
+
+    currentQuestionSpan.textContent = currentQuestionIndex + 1;
+
+    const progressPercent = (currentQuestionIndex / quizQuestions.length) * 100;
+    progressBar.style.width = progressPercent = "%";
+
+    questionText.textContent = currentQuestion.question;
+
+    //explain this in a second
+    answersContainer.innerHTML = '';
+
+
+    currentQuestion.answers.forEach( answer => {
+        const button = document.createElement('button')
+        button.textContent = answer.text
+        button.classList.add('answer-btn');
+
+        // what is dataset?
+        button.dataset.correct = answer.correct ;
+
+        button.addEventListener('click', selectAnswer)
+
+        answersContainer.appendChild(button)
+    })
+}
+
+function selectAnswer(event) {
+    if (answersDisabled) return
+
+    answersDisabled = true;
+
+    const selectedButton = event.target;
+    const iscorrect = selectedButton.dataset.correct === 'true'
+
+    Array.from(answersContainer.children).forEach (button => {
+        if (button.dataset.correct === 'true') {
+            button.classList.add('correct')            
+        } else {
+            button.classList.add('incorrect')
+        }
+    });
+
+    if (iscorrect) {
+        score++;
+        scoreSpan.textContent = score;
+    }
+
+    setTimeout( () => {
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < quizQuestions.length) {
+            showQuestion()
+        }else {
+            showresults()
+        }
+    },1000)
+}
+
+function showresults() {
+    quizScreen.classList.remove('active');
+    quizScreen.classList.add('active');
+
+    finalScoreSpan.textContent = score;
+
+    const precentage = (score/ quizQuestions.length) * 100;
+
+    if(precentage === 100) {
+        resultMessage.textContent = 'Perfect! your\'re a genius! '
+    } else if (precentage === 50) {
+        resultMessage.textContent = 'good job'
+    } else {
+        resultMessage.textContent = 'BRUHHH! 💀'
+    }
+}
+
+
+function restartQuiz() {
+    resultScreen.classList.remove('active');
+    startQuiz();
+}
